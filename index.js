bf94ddea5ecb57bf0db772c2d4fe0c6ad4f819c6
Rohanaitha <aitharohanlucky@gmail.com>
2025-08-12 11:02:03 +0530

    connect changed

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ app.listen(5000, () => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("mongo connected"))
   .catch((err) => {
     console.log("mongo err:", err);
